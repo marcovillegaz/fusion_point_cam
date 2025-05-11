813655df5b4887fbbcd6c513aa4d1979f38cf095
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-05-11 12:25:27 -0400

    camera settings added

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,31 @@
 #include <Arduino.h>
 #include "wifi_setup.h"
 #include "camera_utils.h"
+#include "web_server.h"
 
-const char *ssid = "yourSSID";
-const char *password = "yourPASS";
-IPAddress staticIP(192, 168, 1, 184); // Optional static IP
+const char *ssid = "VTR-1334483";
+const char *password = "Vq3pqvcxhMcy";
+// IPAddress staticIP(192, 168, 1, 184); // Optional static IP
+IPAddress local_IP(0, 0, 0, 0); // forces DHCP instead of static
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(115200); // Initialize serial communication
     delay(1000);
-    connectToWiFi(ssid, password, staticIP);
-    // Setup camera or web server next
 
-    if (!initCamera())
-    {
-        Serial.println("Camera failed to initialize");
-        while (true)
-            ; // Halt
-    }
+    connectToWiFi(ssid, password, local_IP); // Connect to WiFi
+    // testWebServer();                    // Setup web server (shows a simple HTML page)
+
+    // Initialize camera
+    initCamera(); // add try and catch
 
     Serial.println("Camera ready");
+
+    // Start web server
+    setupWebServer();
 }
 
 void loop()
 {
-    // Your main logic
+    handleWebRequests();
 }
