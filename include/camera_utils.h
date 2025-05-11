906b72eb141dba25ad4fa3d623610670d3ad9fbb
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-05-11 10:32:09 -0400

    now pragame once is used

--- include/camera_utils.h
+++ include/camera_utils.h
@@ -1,11 +1,8 @@
 // include/camera_utils.h
-#ifndef CAMERA_UTILS_H
-#define CAMERA_UTILS_H
+#pragma once
 
 #include <esp_camera.h>
 #include <arduino.h>
 
 // Initializes the camera and returns true if successful
 bool initCamera();
-
-#endif
