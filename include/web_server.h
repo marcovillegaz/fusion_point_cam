c06ecf110a5acfc82acbfe9cb4c3250eb29e1c3f
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-05-11 10:32:27 -0400

    specific web server header

---
+++ include/web_server.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <WebServer.h>
+
+extern WebServer server; // WebServer instance
+
+void testWebServer();
+void setupWebServer();
+void handleWebRequests();
+// void startWebServer();
