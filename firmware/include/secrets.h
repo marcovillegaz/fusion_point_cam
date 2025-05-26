bcdd13e5e3733314761f2ebf9074dcbc810b7cd6
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-05-26 19:51:50 -0400

    now the firmware has an independent folder.

---
+++ firmware/include/secrets.h
@@ -0,0 +1,14 @@
+// include/secrets.h
+#pragma once
+
+// Home wifi network
+/* const char *ssid = "VTR-1334483";
+const char *password = "Vq3pqvcxhMcy"; */
+
+// USACH wifi network
+/* const char *ssid = "USACH-Alumnos";
+const char *password = "alumnousach"; */
+
+// Personal wifi network
+const char *ssid = "HONOR X9c Smart";
+const char *password = "1234hola";
