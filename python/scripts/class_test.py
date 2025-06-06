1524591b480212d7fda9a9872820cd13db1b2aed
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-06-06 12:42:42 -0400

    script to test classes

--- python/scripts/class_test.py
+++ python/scripts/class_test.py
@@ -1,34 +1,42 @@
+import os
 from src.steps import *
 from src.postprocess import *
 from classes.imagePipeline import *
 
+experiment = "test4"
 
-# Define your pipeline steps
+input_folder = os.path.join("data/raw/images", experiment)
+output_folder = os.path.join("data/processed", experiment)
+
+
+# Define your pipeline steps THis is only image processing
 steps = [
     ("cropped", lambda img: crop_image(img, x=200, y=0, width=400, height=600)),
     ("greyscale", to_grayscale),
     ("normalize", normalize_clahe),
     ("blurred", apply_blur),
-    ("histogram", lambda img: extract_white_histogram(img, white_threshold=0)),
+    # ("histogram", lambda img: extract_white_histogram(img, white_threshold=0)),
 ]
 
 # Optional prefilter
 prefilter = lambda img: filter_bright_image(img, brightness_threshold=70)
 
+# optional post process (extract data from the final image)
 
-# Optional postprocess
+
+""" # Optional postprocess
 def save_metrics(metrics):
     return mean_median_brightness(
         metrics, save_path=os.path.join(output_folder, "metrics", "mean_median.csv")
-    )
+    ) """
 
 
 # Instantiate and run pipeline
 pipeline = ImagePipeline(
+    experiment_name=experiment,
     steps=steps,
     prefilter_fn=prefilter,
-    postprocess_fn=save_metrics,
-    save_intermediate=False,
+    save_intermediate=True,
 )
 
 pipeline.run(input_folder, output_folder)
