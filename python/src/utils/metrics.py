500c77636645d96b3cff76ca4dafc1b09c3dbc63
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-06-07 23:35:53 -0400

    contrast and sharpeness laplacian mmetric added

--- python/src/utils/metrics.py
+++ python/src/utils/metrics.py
@@ -1,11 +1,8 @@
 """This scripts contains function that extract metrics from images.
 This functions are used in ImagePipeline Instances"""
 
-import os
 import numpy as np
-import pandas as pd
 import cv2
-from src.utils.io import save_metric
 
 # Metrics return a dict with the metric name as key
 
@@ -21,3 +18,12 @@ def edge_density(img):
     edges = cv2.Canny(img, 100, 200)
     density = edges.sum() / edges.size
     return {"edge_density": density}
+
+
+def contrast_rms(img):
+    return {"contrast_rms": float(np.std(img))}
+
+
+def sharpness_laplacian(img):
+    laplacian_var = cv2.Laplacian(img, cv2.CV_64F).var()
+    return {"sharpness": float(laplacian_var)}
