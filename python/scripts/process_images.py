25fe4c217592f9d1a3b16fce526dc65071fa1b60
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-06-07 22:45:19 -0400

    the new postprocess pipeline is applied

--- python/scripts/process_images.py
+++ python/scripts/process_images.py
@@ -2,18 +2,21 @@ import os
 from src.classes.imagePipeline import ImagePipeline
 
 from src.utils.steps import *
-from src.utils.postprocess import *
+from obsolete.postprocess import *
 from src.utils.prefilter import filter_bright_image
+
+from src.utils.metrics import *
+
 
 # --- INPUTS ---
 experiment = "test4"
-input_folder = os.path.join("data/raw/images", experiment)
-output_folder = os.path.join("data/processed", experiment)
+input_folder = os.path.join("data", "raw", "images", experiment)
+output_folder = os.path.join("data", "processed", experiment)
 
 # Define your filter for images
 prefilter = lambda img: filter_bright_image(img, brightness_threshold=70)
 
-# Define your image processing pipeline
+# Define your image processing pipeline (OrderedDtct)
 steps = [
     ("cropped", lambda img: crop_image(img, x=200, y=0, width=400, height=600)),
     ("greyscale", to_grayscale),
@@ -21,16 +24,11 @@ steps = [
     ("blurred", apply_blur),
 ]
 
-# Define your postprocessing pipeline
-postprocess = lambda results, out_folder: mean_median_brightness(results, out_folder)
-
-
-# optional post process (extract data from the final image)
-def save_metrics(metrics):
-    return mean_median_brightness(
-        metrics, save_path=os.path.join(output_folder, "metrics", "mean_median.csv")
-    )
-
+# Define your postprocessing pipeline (list)
+postprocess = [
+    brightness_stats,
+    edge_density,
+]
 
 # Instantiate and run pipeline
 pipeline = ImagePipeline(
