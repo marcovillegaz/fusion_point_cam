f7b3a4d817a653d80f37d7f2e73d2519d444f199
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-11-08 15:11:25 -0300

    feat(package): now des-sle is a package

---
+++ python/modeling/src/des_sle/__init__.py
@@ -0,0 +1,12 @@
+"""
+Deep Eutectic Solvent Solid-Liquid Equilibria Modeling
+"""
+
+__version__ = "0.1.0"
+
+from des_sle.data.component import Component
+from des_sle.models.activity_model import ActivityModel
+from des_sle.models.nrtl import NRTL
+
+# defines which symbols are exported when someone imports *
+__all__ = ["Component", "ActivityModel", "NRTL"]
