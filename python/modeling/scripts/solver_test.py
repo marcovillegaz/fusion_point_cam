28f417c6557798781aa1a4ce052336cf66b94b8a
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-12-05 18:14:56 -0300

    feat: new io function used

--- python/modeling/scripts/solver_test.py
+++ python/modeling/scripts/solver_test.py
@@ -1,61 +1,26 @@
 """Test of solving the SLE equation"""
 
 import numpy as np
-import pandas as pd
 
-from des_sle.models.nrtl import NRTL
-from des_sle.data.component import Component
+from des_sle.io.csv_loader import load_phase_diagram_csv
+from des_sle.io.yaml_loader import load_mixture
+
+from des_sle.thermo.nrtl import NRTL
+
+from des_sle.sle.sle_solver import SLESolver
+from des_sle.sle.sle_equation import SLEEquation
+
 from des_sle.plot.diagrams import plot_sle_diagram
 
-from des_sle.solver.sle_solver import SLESolver
-from des_sle.solver.sle_equation import SLEEquation
-
 # Load experimental data
-solidus_data = pd.read_csv("examples/solidus.csv", sep=";", decimal=",")
-liquidus_data = pd.read_csv("examples/liquidus.csv", sep=";", decimal=",")
+x_solidus, T_solidus = load_phase_diagram_csv("examples/solidus.csv")
+x_liquidus, T_liquidus = load_phase_diagram_csv("examples/liquidus.csv")
 
-# Convert temperatures from °C to K
-solidus_data["T"] = solidus_data["T"] + 273.15
-liquidus_data["T"] = liquidus_data["T"] + 273.15
-
-print(liquidus_data)
-print(solidus_data)
-
-# Each CSV must have two columns: "x" and "T"
-x_solidus = solidus_data["x"].values
-T_solidus = solidus_data["T"].values
-
-x_liquidus = liquidus_data["x"].values
-T_liquidus = liquidus_data["T"].values
-
-
-# Define components with their properties
-Pe4NBr = Component(
-    name="Pe4NBr",
-    Tm=373.2,  # K
-    dHfus=41.45 * 1000,  # J/mol
-    MW=378.47,  # g/mol
-    formula="[CH3(CH2)4]4N(Br)",
-)
-
-erythritol = Component(
-    name="Erythritol",
-    Tm=392.2,  # K
-    dHfus=39.40 * 1000,  # J/mol
-    MW=122.12,  # g/mol
-    formula="C4H10O4",
-)
-
-# Pe4NBr with erythritol (ET)
-params = {
-    "g12": -1567.0,
-    "g21": -4735.4,
-    "alpha12": 0.3,  # Non-randomness parameter
-}
+# Load mixture definition (e.g. Pe4NBr + Erythritol)
+components, params = load_mixture("examples/data/mixtures.yaml", "Pe4NBr_PimelicAcid")
 
 # thermodynamic model isntance
 model = NRTL(params)
-components = [Pe4NBr, erythritol]
 
 # Intance of SLEEquation
 sle_eq = SLEEquation(components, model)
