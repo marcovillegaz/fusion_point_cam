1aacc7cd239b22b008b28bd4c64bc8fa64a1af35
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-12-06 00:05:42 -0300

    chore: init modification

--- python/modeling/src/des_sle/__init__.py
+++ python/modeling/src/des_sle/__init__.py
@@ -5,8 +5,8 @@ Deep Eutectic Solvent Solid-Liquid Equilibria Modeling
 __version__ = "0.1.0"
 
 from des_sle.data.component import Component
-from des_sle.models.activity_model import ActivityModel
-from des_sle.models.nrtl import NRTL
+from des_sle.thermo.activity_model import ActivityModel
+from des_sle.thermo.nrtl import NRTL
 
 # defines which symbols are exported when someone imports *
 __all__ = ["Component", "ActivityModel", "NRTL"]
